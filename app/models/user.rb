60a06f723d0b16ece7b88e346aada570e45e7973
Mattiabzz <mattia.bozzo@edu.unife.it>
2024-03-30 16:10:00 +0100

    aggiornate le relazioni

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    #una persona può partecipare a più eventi 
+    has_many: reservation 
+    has_many: event,
+through: reservation
+ #una persona ha più notifiche 
+ has_many: notify_user
+ has_many: event,
+through notify_user
 end
