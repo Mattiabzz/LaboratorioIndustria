c08455785db421150720d6cdd797ff0eae5adaf5
Mattiabzz <mattia.bozzo@edu.unife.it>
2024-03-30 16:26:37 +0100

    correte le relazioni fra tabelle

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,15 +2,15 @@ class Event < ApplicationRecord
   #un evento ha un solo organizatore
   belongs_to :manager
   #un evento ha più prenotazioni
-  has_many: reservation
-  has_many: user,
-through reservation
+  has_many :reservation
+  has_many :user,
+  :through => :reservation
   #un evento ha più notifiche per gli utenti
-  has_many: notify_user
-  has_many: user,
-through notify_user
+  has_many :notify_user
+  has_many :user,
+:through => :notify_user
  #un evento ha più notifiche per i manager
- has_many: notify_manager
- has_many: manager,
-through notify_manager
+ has_many :notify_manager
+ has_many :manager,
+:through => :notify_manager
 end
--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,8 +1,8 @@
 class Manager < ApplicationRecord
 #un manager ha più eventi 
-has_many: event
+has_many :event
 # un manager può avere più notifiche 
-has_many: notify_manager
-has_many: event,
-through notify_manager
+has_many :notify_manager
+has_many :event,
+:through => :notify_manager
 end
